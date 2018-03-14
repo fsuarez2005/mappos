083bbbd7a6d53c3d7a5eed435bf65190d1307584
Frank Suarez <fsuarez2007@gmail.com>
2018-03-14 19:22:56 -0400

    brainstorming classes

---
+++ mappos/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace mappos
+{
+
+    // 
+    class Product
+    {
+        private int upc;
+        private String description;
+        private String name;
+
+
+
+
+    }
+}
