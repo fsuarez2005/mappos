f429f3b4666194fd2079fec9e0fd0042fb0fcc8a
Frank Suarez <fsuarez2007@gmail.com>
2024-05-17 19:19:04 -0400

    saving

--- java/app/src/main/java/person/franksuarez/MapPOS/model/ProductDatabase.java
+++ java/app/src/main/java/person/franksuarez/MapPOS/model/ProductDatabase.java
@@ -1,6 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * TASKS:
+ * TODO: create standard database methods
+ * TODO: create Database interface
+ * 
+ * 
+ * 
  */
 package person.franksuarez.MapPOS.model;
 
