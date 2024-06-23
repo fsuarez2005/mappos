fbd305f720832bb374e7d7e39b236f7d86a2af50
Frank Suarez <fsuarez2007@gmail.com>
2024-06-22 23:27:38 -0400

    extracting client from old app project

---
+++ java/client/src/main/java/person/franksuarez/MapPOS/client/beans/POS.java
@@ -0,0 +1,38 @@
+
+package person.franksuarez.MapPOS.client.beans;
+
+import java.beans.*;
+
+
+/**
+ *
+ * @author franksuarez
+ */
+public class POS implements java.io.Serializable {
+    private person.franksuarez.MapPOS.common.model.POS posModel;
+    
+    
+    private int currentTime = 0;
+    private String name = "";
+    
+    public int getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(int currentTime) {
+        this.currentTime = currentTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+    
+    
+}
