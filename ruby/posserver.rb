4c7cefd32bed2985c836f57ba15306fcee59ee44
Frank Suarez <fsuarez2007@gmail.com>
2024-11-18 12:00:40 -0500

    ruby experiment

--- ruby/posserver.rb
+++ ruby/posserver.rb
@@ -1,16 +1,26 @@
 #!/usr/bin/env ruby
 
+# TODO: List 
+# [] command list
+# [] transaction process
+# [] 
+
 require 'socket'
+
+def process_connection
+end
 
 
 def simple_server_loop
   server = TCPServer.open(2000)
+  puts "Server: #{server.addr}"
+  
   
   loop {
-    client = server.accept
-    puts "New connection"
+    client, client_address = server.accept
+    puts "New connection from #{client.peeraddr}"
     
-    client.puts "Hello"
+    client.puts "Hello, #{client.peeraddr}. This is #{client.addr}."
 
     client.close
   }
