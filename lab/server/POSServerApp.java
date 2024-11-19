11886b699ae28056aa754e2cd50e65a6b59b4326
Frank Suarez <fsuarez2007@gmail.com>
2024-11-18 20:34:04 -0500

    adding basic console input. moving to root directory.

--- lab/server/POSServerApp.java
+++ lab/server/POSServerApp.java
@@ -1,7 +1,7 @@
 // TODO: List
 // [] Server thread
 // [] Console thread
-
+// [] 
 
 import java.net.*;
 import java.io.*;
@@ -9,6 +9,9 @@ import java.io.*;
 /** Console reads commands from STDIN and processes them.
  */
 class Console implements Runnable {
+    private String prompt = "> ";
+    
+    
     public void processInput(String userInput) {
         
         
@@ -17,16 +20,26 @@ class Console implements Runnable {
     
     
     public void run() {
-        BufferedInputStream bis = new BufferedInputStream(System.in);
+        //BufferedInputStream bis = new BufferedInputStream(System.in);
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(
+                System.in));
+        
+        
         
         System.out.println("Console run");
         boolean running = true;
-        /*
+        
         while (running) {
             // print prompt
-            
+            System.out.printf("%s",this.prompt);
             // get input
-            
+            try {
+                String userInput = in.readLine();
+            } catch (IOException ex) {
+                System.out.printf("Error getting input: %s%n",ex.toString());
+                
+            }
             // process input
             
             
@@ -34,7 +47,6 @@ class Console implements Runnable {
             
             
         }
-        */
         
         
     }
@@ -68,7 +80,6 @@ public class POSServerApp {
         
         POSServerApp p = new POSServerApp();
         System.out.println(p.banner);
-        
         
         // start console thread
         Console c = new Console();
